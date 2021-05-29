2eb36357a4efa142b95d5eccd84c56697f2e987a
Unsleeping <mandrov.ad17@physics.msu.ru>
2021-05-30 02:06:18 +0300

    error handling

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -1,11 +1,30 @@
+import { User } from '../models/User';
+
 export class UserForm {
-  constructor(public parent: Element) {}
+  constructor(public parent: Element, public model: User) {
+    this.bindModel();
+  }
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      'click:button': this.onButtonClick,
+      'click:.set-age': this.onSetAgeClick,
+      'click:.set-name': this.onSetNameClick,
     };
   }
+
+  onSetAgeClick = (): void => {
+    this.model.setRandomAge();
+  };
+
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector('input');
+
+    if (input) {
+      const name = input.value;
+
+      this.model.set({ name });
+    }
+  };
 
   bindEvents(fragment: DocumentFragment): void {
     const eventsMap = this.eventsMap();
@@ -19,20 +38,27 @@ export class UserForm {
     }
   }
 
-  onButtonClick(): void {
-    console.log('12312');
+  bindModel(): void {
+    this.model.on('change', () => {
+      this.render();
+    });
   }
 
   template(): string {
     return `
       <div>
         <h1>User Form</h1>
+        <div>User name: ${this.model.get('name')}</div>
+        <div>User age: ${this.model.get('age')}</div>
         <input />
-        <button>Click Me</button>
+        <button class="set-name">Change Name</button>
+        <button class="set-age">Set random age</button>
       </div>
     `;
   }
   render(): void {
+    this.parent.innerHTML = '';
+
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
 
