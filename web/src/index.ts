b9dd5f4514fe808d3343963df53d0d14e8207fdf
Unsleeping <mandrov.ad17@physics.msu.ru>
2021-06-30 03:52:16 +0300

    CollectionView implementation

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,14 +1,19 @@
-import { UserEdit } from './views/UserEdit';
-import { User } from './models/User';
+import { Collection } from './models/Collection';
+import { UserList } from './views/UserList';
+import { UserProps, User } from './models/User';
 
 export const API_URL = 'http://localhost:3000';
 
-const user = User.buildUser({ name: 'NAME', age: 20 });
-const root = document.getElementById('root');
-if (root) {
-  const userEdit = new UserEdit(root, user);
+const users = new Collection(API_URL, (json: UserProps) => {
+  return User.buildUser(json);
+});
 
-  userEdit.render();
-} else {
-  throw new Error('Root element not found');
-}
+users.on('change', () => {
+  const root = document.getElementById('root');
+
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
