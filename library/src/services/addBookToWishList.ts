84887485589368f15b2f621b5a2e92f2a2bc9e79
Unsleeping <mandrov.ad17@physics.msu.ru>
2021-10-16 18:47:32 +0500

    minor fix

--- library/src/services/addBookToWishList.ts
+++ library/src/services/addBookToWishList.ts
@@ -2,7 +2,7 @@ import { API_URL } from '../constants/constants';
 
 export const addBookToWishList = async (body: any) => {
   try {
-    const rawResponse = await fetch(`${API_URL}/api/updateAccountData`, {
+    const rawResponse = await fetch(`${API_URL}/api/v1/book/wish`, {
       method: 'POST',
       headers: {
         'content-type': 'application/json',
